895700f15e6d55195a0f75fcdb1c9d883c39ae14
Michael Colon <michaelacolon19@gmail.com>
2022-10-20 16:25:06 -0400

    Added Enemy and Lives Code

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,19 +9,28 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rd2d;
     public float speed;
     public Text score;
+    public Text lives;
     private int scoreValue = 0;
-    public int jumpForce = 3;
+    private int livesValue = 3;
+    public float jumpForce = 3;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        lives.text = livesValue.ToString();
+        winTextObject.SetActive(false);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(scoreValue == 4)
+        {
+            winTextObject.SetActive(true);
+        }
         float hozMovement = Input.GetAxis("Horizontal");
         float verMovement = Input.GetAxis("Vertical");
 
@@ -36,6 +45,20 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+        }
+
+        if(collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+        if(livesValue <= 0)
+        {
+            winTextObject.GetComponent<TMPro.TextMeshProUGUI>().text = "Michael Colon says Game Over!";
+            winTextObject.SetActive(true);
+            Destroy(this);
         }
     }
 
